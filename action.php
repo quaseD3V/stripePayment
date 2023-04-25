be6aa8a5ff52ec3108197fe98a93fc412a6a02e4
CARLOS LACERDA DOS SANTOS <carlos_lacerda_dos_santos@outlook.com>
2023-04-25 19:34:11 -0300

    Update action.php

--- action.php
+++ action.php
@@ -4,7 +4,7 @@ namespace action;
 use Stripe\Terminal\Location;
 require 'vendor/autoload.php';
 //   
-\Stripe\Stripe::setApiKey('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');
+\Stripe\Stripe::setApiKey('put your sk_stripe here');
 $charge = \Stripe\Charge::create([
   'source' => $_POST['stripeToken'],
   'description' => 'New Song of New Age',
