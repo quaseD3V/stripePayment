749cdc9fe466dc1474ae52a99c12ddb40748d55d
quaseD3V <carlos_lacerda_dos_santos@outlook.com>
2023-04-25 19:59:42 -0300

    edite sk_stripe

---
+++ action.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace action;
+
+use Stripe\Terminal\Location;
+
+require 'vendor/autoload.php';
+//   
+\Stripe\Stripe::setApiKey('yor sk_live_stripe here');
+$charge = \Stripe\Charge::create([
+  'source' => $_POST['stripeToken'],
+  'description' => 'New Song of New Age',
+  'amount' => 250,
+  'currency' => 'usd'
+]);
+session_start();
+$_SESSION['token'] = $_POST['stripeToken'];
+header('Location: dllink.php');
+
+exit;
